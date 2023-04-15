6a75185e37c29a15b093dc49ebb5aecf6804b4eb
WanderningMaster <user12341528@gmail.com>
2023-04-16 01:51:10 +0300

    ref: Moved models to separate folder

--- compose-prisma-schema.bash
+++ compose-prisma-schema.bash
@@ -1,8 +1,8 @@
 mkdir -p ./src/db/temp
 
-for file in ./src/db/*.chunk.prisma; do
+for file in ./src/db/models/*.prisma; do
     sed '/\/\/@relations/,$d' "$file" > "./src/db/temp/$(basename "$file")"
 done
 
-cat ./src/db/root.prisma ./src/db/temp/*.chunk.prisma > ./src/db/schema.prisma
+cat ./src/db/root.prisma ./src/db/temp/*.prisma > ./src/db/schema.prisma
 rm -rf ./src/db/temp
